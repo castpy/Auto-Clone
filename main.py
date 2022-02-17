72a813e795e06358509ee464d3f082c578a6e44d
Marcus Castilho <rootmarcus001@gmail.com>
2022-02-17 11:05:25 -0300

    Criando diretório com TITLE

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
+import os #Chamando funçoes do sistema
 import csv #Para escrever em arquivo CSV
 import urllib.request #Para fazer request
 from bs4 import BeautifulSoup #Função para fazer analise de dados do site
 
-url = 'https://c4st1lh0.github.io/portifolio/index.html' #Especificando URL 
+url = 'https://www.facebook.com/' #Especificando URL 
 page = urllib.request.urlopen(url) #Indo ao site e armazenando o HTML na variavel
 soup = BeautifulSoup(page, 'html5lib') #Fazendo parse no HTML da variavel e armazenando em formato BFS
+tituloSite = soup.title.string #Buscando TITULO do site
 soup.encode('utf-8') #Codificando para UTF-8
 
 
 f = csv.writer(open('index.html', 'w', encoding='UTF-8')) #Criando o arquivo index.html com o método de escrita 'w'
 f.writerow([soup]) #Escrevendo o conteúdo do site no arquivo index.html
+
+
+dir = tituloSite #Adicionando titulo do site como nome do diretório
+os.mkdir(dir) #Criando o diretório com o titulo do site
 
 
 #  ATENÇÃO!
