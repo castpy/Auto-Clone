2356823396161e729c245cdc243d0a9cb0bfc7e8
Marcus Castilho <rootmarcus001@gmail.com>
2022-02-17 16:48:25 -0300

    Tratando erros

--- main.py
+++ main.py
@@ -18,56 +18,56 @@ while True:
 |/     \|(_______)   )_(   (_______)    (_______/(_______/(_______)|/    )_)(_______/
 \033[m                                          \033[35mBy: Marcus Castilho\033[m v0.1
 ''')
-    op = int(input('''
-    \033[33m[01]\033[m Complete clone
-    \033[33m[02]\033[m Clone HTML
-    \033[33m[03]\033[m Clone CSS
-    \033[31m[04]\033[m EXIT
+    try:
+        op = int(input('''
+\033[33m[01]\033[m Complete clone
+\033[33m[02]\033[m Clone HTML
+\033[33m[03]\033[m Clone CSS
+\033[31m[04]\033[m EXIT
 
-    └>'''))
+└>'''))
+        if op == 1:
+            print('\033[31mEM DESENVOLVIMENTO...\033[m')
+            sleep(2)
+            clear.cls()
+            continue
 
-    if op == 1:
-        print('\033[31mEM DESENVOLVIMENTO...\033[m')
-        sleep(2)
-        clear.cls()
-        continue
+        elif op == 2:
+            try:
+                url = input('Alvo do Clone: ').lower().strip()
+                url = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'})
+                codigoCorpoPage = urllib.request.urlopen(url)
+                soup = BeautifulSoup(codigoCorpoPage, features="html5lib")
+                titleSite = soup.title.string
+                
+                criarPage = open('index.html', 'w', encoding='utf-8')
+                criarPage.write(str(soup))
+                criarPage.close()
 
-    elif op == 2:
-        try:
-            url = input('Alvo do Clone: ').lower().strip()
-            url = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'})
-            codigoCorpoPage = urllib.request.urlopen(url)
-            soup = BeautifulSoup(codigoCorpoPage, features="html5lib")
-            titleSite = soup.title.string
+                diretorio = titleSite
+                os.mkdir(diretorio)
+                shutil.copy2('index.html', diretorio)
+                os.remove('index.html')
+                print('\033[32mSucesso!\033[m')
+                sleep(1)
+                print(f'Verifique o arquivo \033[33mindex.html\033[m no diretorio \033[35m{diretorio}\033[m.')
+                sleep(5)
+            except FileExistsError as error:
+                print('Erro: {}'.format(error))
+                os.remove('index.html')
+                sleep(5)
 
-            criarPage = open('index.html', 'w', encoding='utf-8')
-            criarPage.write(str(soup))
-            criarPage.close()
+        elif op == 3:
+            print('\033[31mEM DESENVOLVIMENTO...\033[m')
+            sleep(2)
+            clear.cls()
+            continue
 
-            diretorio = titleSite
-            os.mkdir(diretorio)
-            shutil.copy2('index.html', diretorio)
-            os.remove('index.html')
-            print('\033[32mSucesso!\033[m')
-            sleep(1)
-            print(f'Verifique o arquivo \033[33mindex.html\033[m no diretorio \033[35m{diretorio}\033[m.')
-            sleep(5)
-        except FileExistsError as error:
-            print('Erro: {}'.format(error))
-            os.remove('index.html')
-            sleep(5)
-
-    elif op == 3:
-        print('\033[31mEM DESENVOLVIMENTO...\033[m')
-        sleep(2)
-        clear.cls()
-        continue
-
-    elif op == 4:
-        clear.cls()
-        break
-    
-    else:
-        print('\033[31mOPÇÃO INVÁLIDA!\033[m')
-        sleep(1)
-        continue
+        elif op == 4:
+            clear.cls()
+            break
+        
+    except ValueError:
+            print('\033[31mOPÇÃO INVÁLIDA! Tente usar números\033[m')
+            sleep(2)
+            continue
