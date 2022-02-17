b64b1df68a080377ead7649907a32b898c9d979a
Marcus Castilho <rootmarcus001@gmail.com>
2022-02-17 15:57:20 -0300

    User agente add

--- main.py
+++ main.py
@@ -35,6 +35,7 @@ while True:
     elif op == 2:
         try:
             url = input('Alvo do Clone: ').lower().strip()
+            url = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'})
             codigoCorpoPage = urllib.request.urlopen(url)
             soup = BeautifulSoup(codigoCorpoPage, features="html5lib")
             titleSite = soup.title.string
@@ -69,3 +70,4 @@ while True:
     else:
         print('\033[31mOPÇÃO INVÁLIDA!\033[m')
         sleep(1)
+        continue
