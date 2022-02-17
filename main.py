c08580eda1ea752f9a068cec13c8c482a16d6d77
Marcus Castilho <rootmarcus001@gmail.com>
2022-02-17 12:05:55 -0300

    Criação de Diretórios e Movimentação de index

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os #Chamando funçoes do sistema
 import csv #Para escrever em arquivo CSV
+import shutil #Para copiar arquivos
 import urllib.request #Para fazer request
+from time import sleep #Para fazer uma contagem em segundos
 from bs4 import BeautifulSoup #Função para fazer analise de dados do site
 
 url = 'https://www.facebook.com/' #Especificando URL 
@@ -17,6 +19,8 @@ f.writerow([soup]) #Escrevendo o conteúdo do site no arquivo index.html
 dir = tituloSite #Adicionando titulo do site como nome do diretório
 os.mkdir(dir) #Criando o diretório com o titulo do site
 
+sleep(1)
+shutil.move('index.html', dir) #Movendo arquivo para o diretório
 
 #  ATENÇÃO!
 #Essa não é a versão final! Esses são os métodos básicos e a base do projeto
