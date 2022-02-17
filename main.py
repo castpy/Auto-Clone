1229632bc37821c834d00e285b60398857c5e156
Marcus Castilho <rootmarcus001@gmail.com>
2022-02-17 14:55:02 -0300

    Versão estável sem css

--- main.py
+++ main.py
@@ -1,26 +1,20 @@
-import os #Chamando funçoes do sistema
-import csv #Para escrever em arquivo CSV
-import shutil #Para copiar arquivos
-import urllib.request #Para fazer request
-from time import sleep #Para fazer uma contagem em segundos
-from bs4 import BeautifulSoup #Função para fazer analise de dados do site
+import os, csv, shutil
+import urllib.request
+from bs4 import BeautifulSoup
 
-url = 'https://www.facebook.com/' #Especificando URL 
-page = urllib.request.urlopen(url) #Indo ao site e armazenando o HTML na variavel
-soup = BeautifulSoup(page, 'html5lib') #Fazendo parse no HTML da variavel e armazenando em formato BFS
-tituloSite = soup.title.string #Buscando TITULO do site
-soup.encode('utf-8') #Codificando para UTF-8
+#url = input('Alvo do Clone: ').lower().strip()
+url = 'https://c4st1lh0.github.io/portifolio/'
+codigoCorpoPage = urllib.request.urlopen(url)
+soup = BeautifulSoup(codigoCorpoPage, features="html5lib")
+titleSite = soup.title.string
 
 
-f = csv.writer(open('index.html', 'w', encoding='UTF-8')) #Criando o arquivo index.html com o método de escrita 'w'
-f.writerow([soup]) #Escrevendo o conteúdo do site no arquivo index.html
+criarPage = open('index.html', 'w', encoding='utf-8')
+criarPage.write(str(soup))
+criarPage.close()
 
+diretorio = titleSite
+os.mkdir(diretorio)
+shutil.copy2('index.html', diretorio)
+os.remove('index.html')
 
-dir = tituloSite #Adicionando titulo do site como nome do diretório
-os.mkdir(dir) #Criando o diretório com o titulo do site
-
-sleep(1)
-shutil.move('index.html', dir) #Movendo arquivo para o diretório
-
-#  ATENÇÃO!
-#Essa não é a versão final! Esses são os métodos básicos e a base do projeto
