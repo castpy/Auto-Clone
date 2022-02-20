ce3367e36ceb74236def6b61ecaa8eaee39841c2
Marcus Castilho <rootmarcus001@gmail.com>
2022-02-20 11:19:18 -0300

    CRIANDO DECOMPILE

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from lib import clear, banner, cloneSite
 from time import sleep
+import os
 
 #Aqui foram feitas as importações necessárias para o bom funcionamento do Programam!
 
@@ -9,7 +10,8 @@ while True:
     try:
         op = str(input('''
 \033[33m[01]\033[m Clone code Site      \033[33mBETA\033[m
-\033[31m[02]\033[m EXIT
+\033[33m[02]\033[m Decompilar APK       \033[33mEM DEV\033[m
+\033[31m[03]\033[m EXIT
 
 └> '''))
     except ValueError:
@@ -21,8 +23,13 @@ while True:
         cloneSite.clone()
         #Essa condição está configurada para clonar somente oque encontrar no HTML
 
+    elif op == '2' or op =='02':
+        path = input('Caminho do arquivo: ')
+        dir = os.path.abspath(path)
+        print(dir)
+        sleep(10)
         
-    elif op == '2' or op == '02': #Essa condição está configurada para terminar o programa
+    elif op == '3' or op == '03': #Essa condição está configurada para terminar o programa
         print('\033[31mVocê Escolheu sair!\033[m')
         sleep(2)
         clear.cls()
