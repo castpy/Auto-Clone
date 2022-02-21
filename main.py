ba1991d45ae2d8e574d35767bb37a043e1420230
Marcus Castilho <rootmarcus001@gmail.com>
2022-02-21 09:47:30 -0300

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,7 @@ while True:
     try:
         op = str(input('''
 \033[33m[01]\033[m Clone code Site      \033[33mBETA\033[m
-\033[33m[02]\033[m Decompilar APK       \033[33mEM DEV\033[m
-\033[31m[03]\033[m EXIT
+\033[31m[02]\033[m EXIT
 
 └> '''))
     except ValueError:
@@ -23,13 +22,7 @@ while True:
         cloneSite.clone()
         #Essa condição está configurada para clonar somente oque encontrar no HTML
 
-    elif op == '2' or op =='02':
-        path = input('Caminho do arquivo: ')
-        dir = os.path.abspath(path)
-        print(dir)
-        sleep(10)
-        
-    elif op == '3' or op == '03': #Essa condição está configurada para terminar o programa
+    elif op == '2' or op == '02': #Essa condição está configurada para terminar o programa
         print('\033[31mVocê Escolheu sair!\033[m')
         sleep(2)
         clear.cls()
